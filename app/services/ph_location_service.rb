096ae6a10eebf51ec464d4fd08dcac5bb9d2a5c9
SunshineRoque <sunshineararoque@gmail.com>
2023-10-16 14:45:32 +0800

    Add: fetch_barangays

--- app/services/ph_location_service.rb
+++ app/services/ph_location_service.rb
@@ -30,7 +30,7 @@ class PhLocationService
   def fetch_districts
     request = RestClient.get("#{url}/districts/")
     district_data = JSON.parse(request.body)
-    district_data .each do |district|
+    district_data.each do |district|
       region = Address::Region.find_by(code: district['regionCode'])
       address_district = Address::Province.find_or_initialize_by(code: district['code'])
       address_district.name = district['name']
@@ -53,4 +53,17 @@ class PhLocationService
       address_city.save
     end
   end
+
+  def fetch_barangays
+    request = RestClient.get("#{url}/barangays/")
+    barangay_data = JSON.parse(request.body)
+    barangay_data.each do |barangay|
+      city_code = barangay['cityCode'] ? barangay['cityCode'] : barangay['municipalityCode']
+      address_barangay = Address::Barangay.find_or_initialize_by(code: barangay['code'])
+      address_barangay.name = barangay['name']
+      address_barangay.city = Address::City.find_by(code: city_code)
+      address_barangay.save
+    end
+  end
+
 end
