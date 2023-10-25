e1fbc2d8f6c9f89f47b149ca01e48faf3ac7f704
SunshineRoque <sunshineararoque@gmail.com>
2023-10-25 14:48:38 +0800

    Add english, chinese mandarin and tagalog to Post model

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,8 @@ module App
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
+    config.i18n.default_locale = :en
+    config.i18n.available_locales = [:en, 'zh-CN', 'tl']
     config.generators.system_tests = nil
   end
 end
