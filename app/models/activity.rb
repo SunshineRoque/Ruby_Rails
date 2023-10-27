444a66c58016e5513a3b9c03ab0997a75e29a824
SunshineRoque <sunshinararoque@gmail.com>
2023-10-27 14:53:55 +0800

    Done using AASM in Activity and Task Model

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,28 @@
 class Activity < ApplicationRecord
   belongs_to :user
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
+  include AASM
+
+  aasm column: 'state' do
+    state :pending, initial: true
+    state :completed, :canceled
+
+    event :cancel do
+      transitions from: :pending, to: :canceled, guard: :all_tasks_canceled?
+    end
+
+    event :complete do
+      transitions from: :pending, to: :completed, guard: :all_tasks_completed?
+    end
+  end
+
+  private
+
+  def all_tasks_completed?
+    tasks.all? { |task| task.completed? || task.canceled? }
+  end
+
+  def all_tasks_canceled?
+    tasks.all? { |task| task.canceled? || task.completed? }
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,23 @@
 class Task < ApplicationRecord
   belongs_to :user
   belongs_to :activity
+  validates :activity, presence: true
+  include AASM
+
+  aasm column: 'state' do
+    state :pending, initial: true
+    state :processing, :completed, :canceled
+
+    event :process do
+      transitions from: :pending, to: :processing
+    end
+
+    event :complete do
+      transitions from: :processing, to: :completed
+    end
+
+    event :cancel do
+      transitions from: :processing, to: :canceled
+    end
+  end
 end
